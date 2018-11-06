14f3323959ad86d481926e09d5e94e038baaf042
rafaelflorindo <rafaelflorindo@hotmail.com>
2018-11-06 11:41:18 -0200

    Add files via upload

--- formPessoa.php
+++ formPessoa.php
@@ -10,3 +10,39 @@
 		<input type="submit" value="CADASTRAR">
 	</form>
 </fieldset>	
+
+
+<?php
+	include("classePessoa.php");
+	$pessoa = new Pessoa();
+	$retorno = $pessoa->listarPessoa();
+
+	$numeroRegistro = count($retorno);
+	
+	if($retorno) {
+	?>
+	<fieldset>
+<legend>Pessoas cadastradas</legend>
+	<table border="1">
+		<th>Código</th>
+		<th>Nome</th>
+		<th>E-mail</th>
+	
+	<?php
+		foreach($retorno as $valor){
+	?>
+	<tr>
+		<td><?php echo $valor["id"];?></td>
+		<td><?php echo $valor["nome"];?></td>
+		<td><?php echo $valor["email"];?></td>
+	</tr>
+		<?php
+	}
+	?>
+	</table>
+	Foram Encontrados <?php echo $numeroRegistro; ?> registros.
+</fieldset>
+<?php
+}
+?>
+
