cb6988b807e755dabd73ab5c7cf9671e21cec370
rafaelflorindo <rafaelflorindo@hotmail.com>
2019-03-26 11:55:50 -0300

    Add files via upload

---
+++ 3info/projetoApp/index.php
@@ -0,0 +1,47 @@
+<html>
+	<head>
+	<title>
+		Projeto App Minha Casa minha Vida
+	</title>
+	<style type="text/css">
+	body{
+		margin:0;
+		padding:0px;	
+		background-color: #DCDCDC;
+	}
+	.topo{
+		width:100%;
+		height: 100px;
+		background-color: #696969;	
+	}
+	.menu{
+		width: 100%;
+		background-color: #DCDCDC;	
+	}
+	.conteudo{
+		width: 70%;
+		margin: 0 auto;	
+		background-color: white;	
+	}
+	.rodape{ 
+		width:100%; height: 150px;
+		background-color: #696969;	
+	}
+	</style>
+	</head>
+<body>
+	<div class="topo">
+		Colocar uma imagem do seu projeto	
+	</div>
+	<div class="menu">
+		Colocar o menu provisório	
+	</div>
+	<div class="conteudo">
+		Conteúdo principal	
+	</div>
+	<div class="rodape">
+		Desenvolvido por Rafael A. Florindo<br>
+		<a href="mailto:rafael.florindo@escola.pr.gov.br">Contato</a>
+	</div>
+</body>
+</html>
