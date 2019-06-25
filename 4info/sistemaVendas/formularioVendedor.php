efe5543d82eb553f344c9f13ec60c0cf68cd46b3
rafaelflorindo <rafaelflorindo@hotmail.com>
2019-06-25 09:51:47 -0300

    Add files via upload

---
+++ 4info/sistemaVendas/formularioVendedor.php
@@ -0,0 +1,6 @@
+<form action="controllerVendedor.php" method="POST">
+	<label>Nome: </label><input type="text" name="nome">
+	<label>E-mail: </label><input type="email" name="email">
+	<label>Salário Fixo: </label><input type="text" name ="salarioFixo">
+	<input type="submit" value="Gravar">
+</form>
