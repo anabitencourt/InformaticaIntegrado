6c32c14dad91c32892cb0b1881c95ced9ffabd78
rafaelflorindo <rafaelflorindo@hotmail.com>
2019-08-12 10:37:38 -0300

    Add files via upload

---
+++ 2info/recebe.php
@@ -0,0 +1,29 @@
+<?php
+    $nome = $_GET["nome"];
+    $email = $_GET["email"];
+    $senha = $_GET["senha"];
+    $endereco = $_GET["endereco"];
+    $cidade = $_GET["cidade"];
+
+    echo "Dados cadastrais";
+    echo "<br>";
+    echo "Nome: $nome";
+    echo "<br>E-mail: $email";
+    echo "<br>Senha: $senha";
+    echo "<br>Endereço: $endereco";
+    echo "<br>Cidade: $cidade";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
