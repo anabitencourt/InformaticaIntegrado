630edbd1c586254ff8ea985570a5f57a831ba68e
rafaelflorindo <rafaelflorindo@hotmail.com>
2019-11-04 09:13:57 -0300

    Add files via upload

--- 3info/3info/AcessoBancoDados/model/Pessoa.php
+++ 3info/3info/AcessoBancoDados/model/Pessoa.php
@@ -13,10 +13,23 @@ class Pessoa{
   public function getEmail(){
     return $this->email;
   }
-
   public function cadastrar($nome, $email){
     $this->setNome($nome);
     $this->setEmail($email);
+    //query de consulta para inclusão
+    echo $sqlInsert = "insert into pessoa
+    (nome, email)
+    values
+    ('{$this->getNome()}','{$this->getEmail()}')";
+    //executar a query
+    include("Conexao.php");
+
+
+    if($retornoInsert = $conectar->query($sqlInsert)){
+      return 1;//retona 1 caso seja gravado
+    }else {
+      return 0;//retona 0 caso não seja gravado
+    }
   }
   public function listar(){
     echo "Nome: " . $this->getNome();
