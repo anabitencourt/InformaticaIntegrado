d2319c6f42b388c69ee02633a78bd339f086bb78
rafaelflorindo <rafaelflorindo@hotmail.com>
2019-11-11 10:24:47 -0300

    Add files via upload

--- 3info/3info/AcessoBancoDados/controler/controle.php
+++ 3info/3info/AcessoBancoDados/controler/controle.php
@@ -4,14 +4,12 @@
 
   $nome = $_POST["nome"];
   $email =$_POST["email"];
+  $dataNascimento = $_POST["dataNascimento"];
 
-  $inserir = $pessoa1->cadastrar($nome, $email);
+  $inserir = $pessoa1->cadastrar($nome, $email, $dataNascimento);
   if ($inserir==1){
     echo "Dados gravados com sucesso";
   }else{
     echo "Erro ao gravar os dados";
   }
-
-  $pessoa1->listar();
-
 ?>
