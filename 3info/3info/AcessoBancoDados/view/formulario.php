06a5071bfe7b35987a74b36d100e8822bd64a82a
rafaelflorindo <rafaelflorindo@hotmail.com>
2019-11-12 11:50:03 -0300

    Add files via upload

--- 3info/3info/AcessoBancoDados/view/formulario.php
+++ 3info/3info/AcessoBancoDados/view/formulario.php
@@ -5,11 +5,22 @@
     <title></title>
   </head>
   <body>
+  <?php
+    if(isset($_GET["mensagem"]) & !empty($_GET["mensagem"])){
+      if($_GET["mensagem"]=="sucesso"){
+        echo "Dados gravados com sucesso!!!";
+      }elseif ($_GET["mensagem"]=="erro") {
+        echo "Erro ao gravar os dados!!!";
+      }
+    }
+  ?>
     <form action="../controler/controle.php" method="post">
       <label>Nome: </label>
       <input type="text" name="nome" value="">
       <label>E-mail:</label>
       <input type="email" name="email" value="">
+      <label>Data Nascimento:</label>
+      <input type="date" name="dataNascimento" value="">
       <input type="submit" name="" value="Enviar">
     </form>
   </body>
